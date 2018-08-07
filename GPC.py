1c4374e6e0b3790c4cc178b49892497b5d19e542
Filippo Castelli <filippocastelli42@gmail.com>
2018-08-07 12:26:36 +0200

    tidied up code

--- GPC.py
+++ GPC.py
@@ -1,8 +1,7 @@
-#from __future__ import division
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as pl
 import scipy.optimize as so
-
 #%%
 def kernel(data1,data2,theta,wantderiv=True,measnoise=1.):
     # Uses exp(theta) to ensure positive hyperparams
@@ -39,16 +38,6 @@ def kernel(data1,data2,theta,wantderiv=True,measnoise=1.):
         return K
     else:
         return k + measnoise*theta[2]*np.eye(d1,d2)
-    
-    
-def old_logPosterior(theta,*args):
-    data,t = args
-    k = kernel(data,data,theta,wantderiv=False)
-    L = np.linalg.cholesky(k)
-    beta = np.linalg.solve(L.transpose(), np.linalg.solve(L,t))
-    logp = -0.5*np.dot(t.transpose(),beta) - np.sum(np.log(np.diag(L))) - np.shape(data)[0] /2. * np.log(2*np.pi)
-    return -logp
-
 
 def logPosterior(theta,*args):
     data,targets = args
@@ -94,23 +83,6 @@ def NRiteration(data,targets,theta):
     #logq = -0.5*np.dot(a.transpose(),f) -np.sum(np.log(ps+np.log(np.exp(-ps) + np.exp(s-ps)))) - np.trace(np.log(L))
     logq = -0.5*np.dot(a.transpose(),f) -np.sum(np.log(ps+np.log(np.exp(-ps) + np.exp(s-ps)))) - sum(np.log(L.diagonal()))
     return (f,logq,a)
-
-
-def oldgradLogPosterior(theta,*args):
-    #print(args)
-    data,t = args
-    theta = np.squeeze(theta)
-    d = len(theta)
-    K = kernel(data,data,theta,wantderiv=True)
-
-    L = np.linalg.cholesky(np.squeeze(K[:,:,0]))
-    invk = np.linalg.solve(L.transpose(),np.linalg.solve(L,np.eye(np.shape(data)[0])))
-	
-    dlogpdtheta = np.zeros(d)
-    for d in range(1,len(theta)+1):
-        dlogpdtheta[d-1] = 0.5*np.dot(t.transpose(), np.dot(invk, np.dot(np.squeeze(K[:,:,d]), np.dot(invk,t)))) - 0.5*np.trace(np.dot(invk,np.squeeze(K[:,:,d])))
-
-    return -dlogpdtheta
 
 def gradLogPosterior(theta,*args):
     data,targets = args
@@ -165,13 +137,9 @@ theta[1] = 0.742025
 #np.random.rand()*3
 theta[2] =-2.125
 
-
 lengthscale = 1
 constantscale = 1
 print("NOW")
-#gradLogPosterior(theta, data, labels)
-#kernel(data, data,theta)
-#newtheta = so.minimize(logPosterior, theta, args=(data,labels), method='CG', jac=gradLogPosterior, tol=None, callback=None, options={'gtol': 1e-05, 'eps': 1.4901161193847656e-08, 'maxiter': 100, 'disp': 1, 'return_all': False})
 newtheta = so.fmin_cg(logPosterior, theta, fprime=gradLogPosterior, args=(data,labels), gtol=1e-4,maxiter=100,disp=1, full_output=0)
 print(newtheta, logPosterior(newtheta,data,labels))
 
@@ -192,11 +160,8 @@ invk = np.linalg.inv(K)
 mean = np.dot(kstar,np.dot(invk,labels))
 var = kstarstar_diag - np.diag(np.dot(kstar,np.dot(invk,kstar.transpose())))
 var = np.reshape(var,(100,1))
-    
 #%%
-
 # PLOTS:
-
 xstar_p = np.squeeze(xstar)
 mean_p = np.squeeze(mean)
 var_p = np.squeeze(np.reshape(var,(len(xstar),1)))
@@ -219,7 +184,6 @@ pl.legend(handles = [latent, datapoints])
 #pl.scatter(test,tlabels)
 pl.savefig('predict.png', bbox_inches='tight')
 pl.axis([-5, 5, -3, 3])
-
 #%%
 pred = np.squeeze(np.array([predict(np.reshape(i,(1,1)),data,labels,newtheta) for i in test]))
 output = np.reshape(np.where(pred[:,0]<0,-1,1),(9,1))
@@ -243,7 +207,6 @@ which = np.where((tlabels==-1) & (output==1))
 test_down_wrong, = pl.plot(test[which],tlabels[which],'gs', label = "test down incorrect")
 pl.legend(handles=[data_up, data_down, test_up_ok, test_down_ok,
                    test_up_wrong, test_down_wrong])
-
 #%%
 pred2 = np.squeeze(np.array([predict(np.reshape(i,(1,1)),data,labels,newtheta) for i in xstar]))
 
@@ -268,30 +231,3 @@ pl.legend(handles = [latent, datapoints])
 #pl.scatter(test,tlabels)
 pl.savefig('predict.png', bbox_inches='tight')
 pl.axis([-5, 5, -3, 3])
-
-##%%
-#L2 = np.linalg.cholesky(kstarstar + 1e-6*np.eye(len(xstar)))
-##f_prior = mu L*N(0,1)
-#f_prior = np.dot(L2, np.random.normal(size=(len(xstar),10)))
-#pl.figure(2)
-#pl.clf()
-#pl.plot(xstar, f_prior)
-#pl.title('10 estrazioni dalla dist. a priori')
-#pl.xlabel('nodes')
-#pl.ylabel('values')
-#pl.axis([-5, 5, -3, 3])
-#pl.savefig('prior.png', bbox_inches='tight')
-##%%
-#Lk = np.linalg.solve(L, kstar.T)
-#L2 = np.linalg.cholesky(kstarstar+ 1e-6*np.eye(n) - np.dot(Lk.T, Lk))
-#
-##f_post = mu + L*N(0,1)
-#f_post = mean.reshape(-1,1) + np.dot(L2, np.random.normal(size=(n,10)))
-#pl.figure(3)
-#pl.clf()
-#pl.plot(xstar, f_post)
-#pl.title('10 estrazioni dalla dist. a posteriori')
-#pl.xlabel('nodes')
-#pl.ylabel('values')
-#pl.axis([-5, 5, -3, 3])
-#pl.savefig('post.png', bbox_inches='tight')
